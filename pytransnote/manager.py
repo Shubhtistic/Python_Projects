9963de29d4366a70af1114fdfe5b072afdf11e8f
Shubham <theshubham1412@gmail.com>
2025-10-14 03:09:10 +0530

    Fixed Bug: name mangling Also changed the Google translator with mymemory we only used google translator for testing purposes so we wont waste our trials of  mymemory googletranslator is unofficial (scraped) so it may stop working anytime

--- pytransnote/manager.py
+++ pytransnote/manager.py
@@ -1,23 +1,23 @@
 
-from deep_translator import GoogleTranslator
+from deep_translator import MyMemoryTranslator
 
 class my_translator():
     def __init__(self):
-        self.__user_name="Guest"
-        self.__from_lang=None
-        self.__to_lang=None
+        self._user_name="Guest"
+        self._from_lang="english"
+        self._to_lang="spanish"
     def set_user_name(self,name):
-        self.__user_name=name
+        self._user_name=name
     def set_to_lang(self,language):
-        self.__to_lang=language
+        self._to_lang=language
     def set_from_lang(self,language):
-        self.__from_lang=language
+        self._from_lang=language
     def translation(self,from_str):
         text_to_translate = from_str
-        ts= GoogleTranslator(source=self.__from_lang, target=self.__to_lang)
+        ts= MyMemoryTranslator(source=self._from_lang, target=self._to_lang)
         return ts.translate(text_to_translate)
     def display_user(self):
-        return self.__user_name
+        return self._user_name
 
             
 
