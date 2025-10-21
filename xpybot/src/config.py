a7a0fd0bedf48c3c48f19bf652a4ef390c17af0c
Shubham <theshubham1412@gmail.com>
2025-10-21 18:23:40 +0530

    feat: add config.py to load secure credentials from .env file

---
+++ xpybot/src/config.py
@@ -0,0 +1,24 @@
+# this module i.e config.py, the main responsibilty is too load our critical info like api keys and othe info from .env file
+# by doing this we can just import config.py and just use this config file in every file we need the credentials
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
+
+# our defined keywords to search for
+KEYWORDS_TO_SEARCH = ["#python", "#developer"]
+MINIMUM_LIKE_COUNT = 20
+
+# to see if all info is loaded properly
+if not all([TWITTER_API_KEY, TWITTER_API_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET]):
+    raise ValueError("Missing one or more required Twitter API v1.0a credentials in .env file")
+
+## the program will not start is there is any error with credentials 
+## raise value error
