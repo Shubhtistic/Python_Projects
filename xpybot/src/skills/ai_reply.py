09a3deadf9f6bcbd3b99db67a80939dd81aa8a2c
Shubham <theshubham1412@gmail.com>
2025-10-24 18:29:58 +0530

    Feat: Initial Implementation of ai_reply ai_reply performs all the replies using ai_client

---
+++ xpybot/src/skills/ai_reply.py
@@ -0,0 +1,46 @@
+from x_client import X_Client
+from ai_client import AI_Client
+from database import Json_DB
+import src.config as cf
+
+class AI_Reply:
+    def __init__(self,x_client:X_Client,ai_client:AI_Client,db:Json_DB):
+        self._x_client=x_client
+        self._ai_client=ai_client
+        self._db=db
+    def perform_replies(self):
+        mentions_list=self._x_client.get_recent_mentions(cf.MAXIMUM_MENTIONS_TO_FIND)
+        if not mentions_list:
+            print("No mentions found, Exiting .....")
+            return
+        for mention in mentions_list:
+            mention_tweet_id = mention["tweet_id"]
+            if(self._db.is_processed(mention_tweet_id)):
+                continue
+            mentioner_username = mention["mentioner_username"]
+            mention_text = mention["tweet_text"]
+
+            print(f"replying to {mentioner_username} using AI ...")
+
+            ai_reply_text = self._ai_client.ai_reply(mention_text)
+
+            if not ai_reply_text:
+                print(f"AI failed to generate reply for tweet {mention_tweet_id}")
+                continue
+            
+            reply_status=self._x_client.reply_to_tweet(reply_text=ai_reply_text,
+                                                       tweet_id_to_reply=mention_tweet_id,
+                                                       mentioner_username=mentioner_username)
+
+    
+            if reply_status:
+                print(f"successfully replied to @{mentioner_username}")
+                # Optionally store the replied tweet ID to avoid duplicate replies
+                self._db.add_processed_id(mention_tweet_id)
+            else:
+                print(f"failed to reply to @{mentioner_username}")
+
+
+
+
+        
