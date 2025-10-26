dd59d85ab38546511bec9b890740fced3bd3f992
Shubham <theshubham1412@gmail.com>
2025-10-26 16:44:39 +0530

    Feat: Implemented log_config to add logging to the project this file has an root global logger object which we can use in any file

---
+++ xpybot/src/log_config.py
@@ -0,0 +1,56 @@
+import logging
+import sys
+
+def logger():
+    log=logging.getLogger()
+    # a root global logger object -> logging.getLogger() with no name
+    # whenever we do log.info() , long.error() 
+    # this single object will send your message to all active handlers
+    # handlers decide where our logs go
+    # you can attach multiple handlers to one logger
+
+    log.setLevel(logging.DEBUG)
+    # by set level we mean catch any message that is higher than Debug
+    # Levels =>  DEBUG < INFO < WARNING < ERROR < CRITICAL
+    # now the logger can take messages we need to tell it where to store or display them
+
+    if log.hasHandlers():
+        log.handlers.clear()
+    #Explanation:-
+    # When we call logg.getLogger() Python gives us the same global logger object for that process
+    # If we call it 10 times during runtime, it’s the same object (same handlers, same configuration).
+    # That’s why we have to clear() handlers — otherwise they pile up within that running process.
+    #Each message we log (like log.info("Hello")) will then be printed multiple times — one copy per handler
+    #log.hasHandlers() -> checks if this logger already has handlers attached.
+    #log.handlers.clear() -> removes all the old ones (reset)
+
+    try:
+        log_file=logging.FileHandler("xpybot.log",mode="a",encoding='utf-8')
+        # filehandler tells the logger to  write all messages to the log file xpybot.log
+
+        log_file.setLevel(logging.DEBUG)
+        # all messaggges debug and above will go in this file
+
+        file_format=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # our decided time format
+
+        log_file.setFormatter(file_format)
+
+        log.addHandler(log_file)
+        #Now every message sent to log.info() log.debug() etc will also be written to xpybot.log.
+    except Exception as e:
+        print("Failed to open log file",file=sys.stderr)
+    try:
+        # sys.stdout is the stream for our console
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.INFO) 
+        
+        console_formatter = logging.Formatter(
+            '%(levelname)s - %(name)s - %(message)s'
+        )
+        console_handler.setFormatter(console_formatter)
+        
+        log.addHandler(console_handler)
+    except Exception as e:
+        print(f"CRITICAL: Failed to set up console logger: {e}", file=sys.stderr)
+    log.info("Logging started")
